cbbaea1960fb3d8521590d128baa2a890b5d984e
rammer7412 <coderammer7412@gmail.com>
2025-09-19 22:01:49 +0900

    Update: Theory of Computation 2

--- _posts/temp/test.scala
+++
@@ -1,7 +0,0 @@
-val list: List[Int] = 3 :: 1 :: 2 :: 4 :: Nil
-def getSecond(list: List[Int]): Int = list match {
-  case _ :: x :: _ => x
-  case _ => 0
-}
-
-getSecond(list) // list의 앞에서부터 2번 째 원소를 꺼내는 함수
